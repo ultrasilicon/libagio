4e463069593ba150745ed0f9c8feca22f626ba8b
ultrasilicon <zhenghanecho@gmail.com>
2019-05-23 11:36:50 -0400

    Commented debug code

--- src/async_event.cpp
+++ src/async_event.cpp
@@ -4,13 +4,13 @@ using namespace Parsley;
 
 void AsyncEvent::executeCb(uv_async_t* handle)
 {
-  auto oldf = &getInstance(handle)->data_->pHandle;
-  static_cast<AsyncEvent*>(static_cast<uv_async_t*>(handle)->data)->onCalled();
+//  auto oldf = &getInstance(handle)->data_->pHandle;
+//  static_cast<AsyncEvent*>(static_cast<uv_async_t*>(handle)->data)->onCalled();
 
 //  auto newf = getPHandle(handle);
 //  newf->onCalled();
 //  getPHandle(handle)->onCalled();
-//  getInstance(handle)->onCalled();
+  getInstance(handle)->onCalled();
 }
 
 AsyncEvent::AsyncEvent(Loop *l)
@@ -27,5 +27,5 @@ int AsyncEvent::send()
 
 void AsyncEvent::print()
 {
-  std::cout << "hello!!!!!!!!!!!!!!!!" << std::endl;
+  std::cout << "TEST AsyncEvent in libparsley" << std::endl;
 }

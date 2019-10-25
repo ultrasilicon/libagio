3c2da5d53977a6d32a0e6534d6b003b176e253e0
ultrasilicon <zhenghanecho@gmail.com>
2019-10-25 10:51:22 -0400

    Completed unit tests

--- include/protocol_utils.h
+++ include/protocol_utils.h
@@ -3,9 +3,14 @@
 
 #include "variant.h"
 
+#include <string>
+#include <vector>
+
 A_NS_BEGIN
 
 namespace ProtocolUtils {
+  using pe_str_len_t = uint32_t;
+
 #pragma pack(1)
   template <typename T>
   struct SizedMask {
@@ -48,7 +53,58 @@ namespace ProtocolUtils {
     stream += sizeof(T);
     return r;
   }
+
+  //template <typename _HeaderT, typename _ValT>
+  //_ValT redeemStr(char* &stream, const char* end)
+  //{
+  //  if(stream == end)
+  //    return "";
+  //  _ValT r = (_ValT)constructStr(stream + sizeof(_HeaderT),
+  //                                 scopeLen<_HeaderT>(stream));
+  //  stream += sizeof(_HeaderT) + scopeLen<_HeaderT>(stream);
+  //  return r;
+  //}
+
+  template <typename _HeaderT>
+  std::string redeemStr(char* &stream, const char* end)
+  {
+    if(stream == end)
+      return "";
+    auto ptr = stream + sizeof(_HeaderT);
+    auto size = scopeLen<_HeaderT>(stream);
+    std::string r(ptr, size);
+    stream += sizeof(_HeaderT) + scopeLen<_HeaderT>(stream);
+    return r;
+  }
+
+  template <typename _ValT>
+  void insertVal(std::vector<char>& stream, size_t& pos, const _ValT& v)
+  {
+    stream.resize(stream.size() + sizeof(_ValT));
+    memcpy(&((SizedMask<_ValT>*) &stream[pos])->header, &v, sizeof(_ValT));
+    pos += sizeof(_ValT);
+  }
+
+  /*!
+   * \brief: If this funciton is not a template, it will have compile errors, because it's not in any class.
+   * Put it here for now as in the future it all these parsing functions will be moved to a pre-compile module.
+   * \arg _std_string must be std::string.
+   */
+  template <typename _std_string>
+  void insertStr(std::vector<char>& stream, size_t& pos, const _std_string& s)
+  {
+    stream.resize(stream.size() + sizeof(pe_str_len_t) + s.length());
+    auto len = s.length();
+    memcpy(&((SizedMask<std::string>*) &stream[pos])->header, &len, sizeof(pe_str_len_t));
+    pos += sizeof(pe_str_len_t);
+    memcpy(&stream[pos], s.c_str(), s.length());
+    pos += s.length();
+  }
+
 }
+
+
+
 
 
 A_NS_END
---
+++ test/test_protocol_utils.cpp
@@ -0,0 +1,180 @@
+#include "protocol_utils.h"
+#include "variant.h"
+#include "gtest/gtest.h"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace Agio::ProtocolUtils;
+
+TEST(TestRedeemVal, SingleLayerPacket)
+{
+  //! [6:hello?]
+  char* s = "hello?";
+  char* stream = (char*) malloc( sizeof(uint32_t) + strlen(s) );
+  char* pos = stream;
+
+  auto header = strlen(s);
+  memcpy(pos, (char*)&header, sizeof(uint32_t));
+  pos += sizeof(uint32_t);
+
+  strcpy(pos, s);
+  pos += strlen(s);
+
+  auto end = scopeEnd<uint32_t>(stream);
+  EXPECT_EQ("hello?", string(redeemStr<uint32_t>(stream, end)));
+}
+
+TEST(TestRedeemVal, OverRedeemVariable)
+{
+  //! [6:hello?]
+  int s = 65535;
+  char* stream = (char*) malloc( sizeof(uint32_t) + sizeof(s) );
+  char* pos = stream;
+
+  auto header = sizeof(s);
+  memcpy(pos, (char*)&header, sizeof(uint32_t));
+  pos += sizeof(uint32_t);
+
+  memcpy(pos, (char*)&s, sizeof(s));
+  pos += sizeof(s);
+
+  auto begin = scopeBegin<uint32_t>(stream);
+  auto end = scopeEnd<uint32_t>(stream);
+  EXPECT_EQ(65535, redeemVal<int>(begin, end));
+}
+
+TEST(TestRedeemVal, MultiLayerPacket)
+{
+  //! [10:[6:hello?]]
+  char* s = "hello?";
+  char* stream = (char*) malloc( sizeof(uint32_t) + sizeof(uint32_t) + strlen(s) );
+  char* pos = stream;
+
+  uint32_t header;
+
+  header =
+      sizeof(uint32_t) + strlen(s) // first cell
+      ;
+  memcpy(pos, (char*)&header, sizeof(uint32_t));
+  pos += sizeof(uint32_t);
+
+  header = strlen(s);
+  memcpy(pos,  (char*)&header, sizeof(uint32_t));
+  pos += sizeof(uint32_t);
+
+  strcpy(pos, s);
+  pos += strlen(s);
+
+  auto begin = scopeBegin<uint32_t>(stream);
+  auto end = scopeEnd<uint32_t>(stream);
+  EXPECT_EQ(string("hello?"), string(redeemStr<uint32_t>(begin, end)));
+}
+
+using variant_t = Agio::Variant<
+    bool
+    , int8_t
+    , int16_t
+    , int32_t
+    , int64_t
+    , uint8_t
+    , uint16_t
+    , uint32_t
+    , uint64_t
+    , std::string
+>;
+
+struct Packet
+{
+  std::vector<variant_t> data;
+  uint8_t msgType;
+};
+
+TEST(TestRedeemVal, MultiLayerMultiCellPacket)
+{
+  //! [20:[6:hello,][6:world!]]
+    std::vector<char> stream(sizeof(uint32_t));
+    size_t pos{};
+
+    Packet pk {{std::string{"hello,"}, std::string{"world!"}, std::string{"duckhacks"}, uint32_t{2019}}, 100};
+
+    *(uint32_t*)&stream[0] = (4 + 6) + (4 + 6) + (4 + 9) + 4 + 1; // set main header
+    pos += sizeof(uint32_t); // skip main header
+
+    insertVal(stream, pos, pk.msgType);
+    insertStr(stream, pos, pk.data[0].get<std::string>());
+    insertStr(stream, pos, pk.data[1].get<std::string>());
+    insertStr(stream, pos, pk.data[2].get<std::string>());
+    insertVal(stream, pos, pk.data[3].get<uint32_t>());
+
+    auto p = scopeBegin<uint32_t>(stream.data());
+    auto end = scopeEnd<uint32_t>(stream.data());
+    EXPECT_EQ(uint8_t{100}, uint8_t(redeemVal<uint8_t>(p, end)));
+    EXPECT_EQ(string("hello,"), string(redeemStr<uint32_t>(p, end)));
+    EXPECT_EQ(string("world!"), string(redeemStr<uint32_t>(p, end)));
+    EXPECT_EQ(string("duckhacks"), string(redeemStr<uint32_t>(p, end)));
+    EXPECT_EQ(uint32_t{2019}, uint32_t(redeemVal<uint32_t>(p, end)));
+}
+
+//TEST(TestEncode, MultiLayerMultiCellPacket)
+//{
+//    Packet pk {{std::string{"hello,"}}, 1};
+
+//    iovec v = ParseEngine::encode(&pk);
+
+//    std::vector<char> stream((char*)v.iov_base, (char*)v.iov_base + v.iov_len);
+
+//    auto p = scopeBegin<uint32_t>(stream.data());
+//    auto end = scopeEnd<uint32_t>(stream.data());
+//    EXPECT_EQ(uint8_t{1}, uint8_t(redeemVal<uint8_t>(p, end)));
+//    EXPECT_EQ(string("hello,"), string(redeemStr<uint32_t>(p, end)));
+//}
+
+TEST(TestRedeemVal, MultiLayerOverRedeemString)
+{
+  //! [19:[6:hello?][5:world]]
+  char* s1 = "hello?";
+  char* s2 = "world";
+  char* stream = (char*) malloc(sizeof(uint32_t) // main header
+                                + sizeof(uint32_t) + strlen(s1) // cell1 total
+                                + sizeof(uint32_t) + strlen(s2) // cell2 total
+                                );
+  char* pos = stream;
+
+  uint32_t header;
+
+  header = // main header
+      sizeof(uint32_t) + strlen(s1) // cell1 total
+      + sizeof(uint32_t) + strlen(s2) // cell2 total
+      ;
+  memcpy(pos, (char*)&header, sizeof(uint32_t));
+  pos += sizeof(header);
+
+  // cell1 header
+  header = strlen(s1);
+  memcpy(pos, (char*)&header, sizeof(uint32_t));
+  pos += sizeof(header);
+
+  // cell1 data
+  strcpy(pos, s1);
+  pos += strlen(s1);
+
+  // cell2 header
+  header = strlen(s2);
+  memcpy(pos, (char*)&header, sizeof(uint32_t));
+  pos += sizeof(header);
+
+  // cell1 data
+  strcpy(pos, s2);
+  pos += strlen(s2);
+
+
+  auto p = scopeBegin<uint32_t>(stream);
+  auto end = scopeEnd<uint32_t>(stream);
+  for(std::string s : {"hello?", "world", "", "", ""})
+    EXPECT_EQ(s, string(redeemStr<uint32_t>(p, end)));
+
+}
+
+

9980491694340a5ba2d05df3fc9e72f84a7049f4
ultrasilicon <zhenghanecho@gmail.com>
2019-11-19 12:58:32 -0500

    Added experimental message parser

--- include/protocol_utils.h
+++ include/protocol_utils.h
@@ -6,6 +6,8 @@
 #include <cstring>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <cstdarg>
 
 A_NS_BEGIN
 
@@ -105,6 +107,115 @@ namespace ProtocolUtils {
 }
 
 
+
+using variant_t = Agio::Variant<
+    bool
+    , int8_t
+    , int16_t
+    , int32_t
+    , int64_t
+    , uint8_t
+    , uint16_t
+    , uint32_t
+    , uint64_t
+    , std::string
+>;
+
+
+struct Packet
+{
+  std::vector<variant_t> data;
+  uint8_t msg_type_;
+};
+
+template <typename... Seq>
+struct MessageScheme {
+  int msg_type_ = 0;
+};
+
+template <typename Scheme, typename... Schema>
+struct Parser {
+  std::unordered_map<int, Variant<Schema...>> schema_;
+
+  Parser(Scheme& scheme) {
+    schema_.insert({scheme.message_type_, scheme});
+  }
+
+  Parser(Scheme& scheme, Schema&... schema) {
+    schema_.insert({scheme.message_type_, scheme});
+    Parser(schema...);
+  }
+
+  Packet* decode(char* stream) {
+    A_USED(stream);
+    return nullptr;
+  }
+
+
+  template<typename T>
+  char* encode_helper(char* pos, T& t) {
+    A_USED(pos);
+    A_USED(t);
+    return pos;
+  }
+
+  template<typename T, typename... Ts>
+  char* encode_helper(char* pos, T& t, Ts&... ts) {
+    A_USED(pos);
+    A_USED(t);
+    return pos;
+  }
+
+  char* encode(Packet* packet) {
+    auto scheme = schema_.at(packet->msg_type_);
+
+
+    return encode_helper(scheme);
+  }
+};
+
+template <typename Scheme, typename... Schema>
+Parser<Scheme, Schema...>* make_parser(Scheme scheme, Schema... schema) {
+  return new Parser<Scheme, Schema...>(scheme, schema...);
+}
+
+template<typename T>
+void expected()
+{
+  MessageScheme<
+      int32_t
+      > schemeHeartbeat{0};
+
+  MessageScheme<
+      std::string
+      > schemeAuthReq{1};
+
+  MessageScheme<
+      std::string
+      > schemeAuthRes{2};
+
+  MessageScheme<
+      std::string
+      > schemeLoginReq{3};
+
+  MessageScheme<
+      bool,
+      std::string
+      > schemeLoginRes{4};
+
+  MessageScheme<
+      std::string,
+      int32_t,
+      std::string
+      > schemeMsgTxt{5};
+
+  auto* parser = make_parser(schemeHeartbeat,
+                        schemeAuthReq,
+                        schemeAuthRes,
+                        schemeLoginReq,
+                        schemeLoginRes,
+                        schemeMsgTxt);
+}
 
 
 

bdc311e2e0343448f53389db3211689c57b29e43
ultrasilicon <zhenghanecho@gmail.com>
2020-01-08 21:03:43 +0800

    Building decode function
    1. Finished parsing logic for ParsingStates::Start
    2. Working on parsing logic for ParsingStates::ReadingBody

--- include/proto_engine.h
+++ include/proto_engine.h
@@ -1,32 +1,79 @@
 #ifndef AGIO_PROTOCOL_PARSER_H
 #define AGIO_PROTOCOL_PARSER_H
 
+#define A_PROTO_ENGINE_HEADER_TYPE uint32_t
+#define A_PROTO_ENGINE_HEADER_SIZE sizeof(A_PROTO_ENGINE_HEADER_TYPE)
+
 #include "agio.h"
 #include "buffer.h"
 #include "proto_utils.h"
 #include "tcp_socket.h"
+#include "async_event.h"
+
+#include <queue>
 
 A_NS_BEGIN
 
 using namespace ProtoUtils;
 
+class ProtoEngine;
+class StreamProtoEngine;
+
+
 class ProtoEngine
 {
 public:
+  using MsgSizeT = A_PROTO_ENGINE_HEADER_TYPE;
   CallbackHandler<void(Packet* packet)> onMessage;
 
-  ProtoEngine(TcpSocket* socket);
-  void message(Packet* packet);
+  ProtoEngine(TcpSocket* sock, Loop* l);
+  virtual ~ProtoEngine();
+
+  virtual void message(Packet* packet) = 0;
+
+protected:
+  AsyncEvent* try_parse_;
+  TcpSocket* socket_;
+  std::queue<Buffer*> buffers_;
+
+  virtual void encode(AsyncEvent* ev) = 0;
+  virtual void decode(AsyncEvent* ev) = 0;
+};
+
+
+
+class StreamProtoEngine
+    : public ProtoEngine
+{  
+public:
+  StreamProtoEngine(TcpSocket* sock, Loop* l);
+  void message(Packet* pkt);
 
 private:
-  std::string buffer_;
-  TcpSocket* socket_;
+  struct ParsingStates {
+    enum Identifier : unsigned char {
+      Start = 0,
+      ReadingHeader,
+      ReadingBody,
+    };
 
-  void read(Buffer* stream, TcpSocket* socket);
-  void write(Packet* packet);
-  Buffer* encode(Packet* packet);
-  Packet* decode(Buffer* );
+    char* rptr = nullptr;   //! pointer to a buffer in ProtoEngine::buffers_ for reading
+    char* wptr = nullptr;   //! pointer to ParsingStates::wbuf for writing
+    Buffer* wbuf;           //! parsing buffer with length equal to the message length
+    MsgSizeT msg_len = 0;  //! message length
+    MsgSizeT read_len = 0; //! read message length
+    Identifier id = Start;  //! state name identifier
+  };
+
+  ParsingStates states_;
+
+  void read(Buffer* stream, TcpSocket* sock);
+  void write(Packet* pkt);
+//  void bufcpy(char* dst, char* src, const size_t& len);
+  void encode(AsyncEvent* ev);
+  void decode(AsyncEvent* ev);
 };
 
 A_NS_END
+
 #endif // AGIO_PROTOCOL_PARSER_H
--- src/proto_engine.cpp
+++ src/proto_engine.cpp
@@ -2,23 +2,111 @@
 
 using namespace Agio;
 
-ProtoEngine::ProtoEngine(TcpSocket* socket)
-  : socket_(socket)
+ProtoEngine::ProtoEngine(TcpSocket* socket, Loop* l)
+  : try_parse_(new AsyncEvent(l))
+  , socket_(socket)
+{
+  on(&try_parse_->onCalled, this, &ProtoEngine::decode);
+}
+
+void StreamProtoEngine::message(Packet* packet)
+{
+  write(packet);
+}
+
+void StreamProtoEngine::decode(AsyncEvent* ev)
+{
+  switch (states_.id) {
+    case ParsingStates::Start:
+      {
+        if(buffers_.size() == 0)
+          return;
+        if(buffers_.front()->length() < sizeof(MsgSizeT)) // header incomplete
+          return;
+
+        states_.msg_len = scopeLen<MsgSizeT>(buffers_.front()->data()); // read header
+        states_.wbuf = new Buffer(states_.msg_len); // allocate sufficient buffer
+        states_.wptr = states_.wbuf->data(); //! TODO apply [] operator?
+        Buffer* rbuf = buffers_.front();
+        if(rbuf->length() < states_.msg_len + sizeof(MsgSizeT))
+          {
+            // read the whole first buffer and destroy
+            MsgSizeT readLen = rbuf->length() - sizeof(MsgSizeT);
+            memcpy(states_.wptr,
+                   scopeBegin<MsgSizeT>(rbuf->data()),
+                   readLen);
+            buffers_.pop();
+            delete rbuf;
+
+            states_.read_len = readLen;
+            states_.rptr = buffers_.front()->data();
+            states_.id = ParsingStates::ReadingHeader;
+          }
+        else
+          {
+            // read part of the buffer
+            memcpy(states_.wptr,
+                   scopeBegin<MsgSizeT>(rbuf->data()),
+                   states_.msg_len);
+
+            states_.read_len = states_.msg_len;
+            states_.rptr = scopeEnd<MsgSizeT>(rbuf->data());
+            states_.id = ParsingStates::ReadingBody;
+          }
+
+        decode(ev);
+        return;
+      }
+    case ParsingStates::ReadingHeader:
+      {
+
+        return;
+      }
+    case ParsingStates::ReadingBody:
+      {
+        if(buffers_.size() == 0 || !states_.rptr || !states_.wptr)
+          return;
+        while (buffers_.size() != 0 && states_.read_len < states_.msg_len) {
+            Buffer* rbuf = buffers_.front();
+            MsgSizeT distance = rbuf->back() - states_.rptr;
+            if(states_.read_len + distance < states_.msg_len)
+              {
+                // read the whole buffer
+                MsgSizeT readLen = rbuf->length() - sizeof(MsgSizeT);
+                memcpy(states_.wptr,
+                       scopeBegin<MsgSizeT>(rbuf->data()),
+                       readLen);
+                buffers_.pop();
+                delete rbuf;
+
+                states_.read_len = readLen;
+                states_.rptr = buffers_.front()->data();
+                states_.id = ParsingStates::ReadingHeader;
+              }
+            else
+              {
+                // read part of the buffer
+                //! TODO
+              }
+          }
+        return;
+      }
+
+    }
+}
+
+
+void StreamProtoEngine::read(Buffer* stream, TcpSocket*)
+{
+  buffers_.push(stream);
+  try_parse_->send();
+}
+
+void StreamProtoEngine::write(Packet* packet)
 {
 }
 
-void ProtoEngine::message(Packet* packet)
+void StreamProtoEngine::encode(AsyncEvent* ev)
 {
-
 }
 
-Buffer* ProtoEngine::encode(Packet* packet)
-{
-
-  return nullptr;
-}
-
-Packet* ProtoEngine::decode(Buffer*)
-{
-  return nullptr;
-}

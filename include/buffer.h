1317f0742f34d3134e8b370400d43c35bfc98209
ultrasilicon <zhenghanecho@gmail.com>
2020-01-28 12:02:06 -0500

    Added uvbuf constructor to Buffer

--- include/buffer.h
+++ include/buffer.h
@@ -8,23 +8,28 @@ A_NS_BEGIN
 
 class Buffer;
 
+/*!
+ * \brief The Buffer class
+ * \note Buffer maintains a iovec-like buffer.
+ * \note Ending character '\0' is regarded as regular character
+ */
 class Buffer
     : public AgioObject<uv_buf_t, Buffer>
 {
 public:
   Buffer(const std::string& data);
   Buffer(const unsigned int& len);
+  Buffer(uv_buf_t* buf);
   Buffer(char* data, const unsigned int& len);
   ~Buffer();
 
-  constexpr char& operator[](const std::size_t &idx);
-  constexpr const char& operator[](const std::size_t &idx) const;
-  constexpr char* front() const;
-  constexpr char* back() const;
-  constexpr char* data() const;
-  constexpr size_t length() const;
-  constexpr size_t size() const;
-  constexpr uv_buf_t* uvBuffer() const;
+  char& operator[](const std::size_t &idx);
+  const char& operator[](const std::size_t &idx) const;
+  char* front() const;
+  char* back() const;
+  char* data() const;
+  size_t length() const;
+  size_t size() const;
 
   std::string toString() const;
   //! \brief copy Copy first len bytes to dest.

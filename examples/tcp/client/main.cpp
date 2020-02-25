11bbef50dca5fb0b9798c754b403a593fe9bcc48
ultrasilicon <zhenghanecho@gmail.com>
2020-02-25 14:00:07 -0500

    Updated examples

--- examples/tcp/client/main.cpp
+++ examples/tcp/client/main.cpp
@@ -1,35 +1,27 @@
 #include <libagio/tcp_socket.h>
 #include <libagio/timer.h>
-
 #include <iostream>
 
 using namespace std;
 using namespace Agio;
 
-static TcpSocket *client;
-
-void timeout_cb(Timer *)
-{  
-  if(client->write("hello") == 0)
-    cout << "written" << endl;
-}
-
-void connected_cb()
-{
-  cout << "connected." << endl;
-}
 
 int main()
 {
   Loop loop;
 
-  client = new TcpSocket(&loop);
-  client->connect("127.0.0.1", 63773);
-  on(&client->onConnected, &connected_cb);
+  TcpSocket *client = new TcpSocket(&loop);
+  on(&client->onConnected, [](){
+      cout << "connected." << endl;
+    });
 
   Timer *timer = new Timer(2000, 500, &loop);
-  on(&timer->onTimedOut, &timeout_cb);
+  on(&timer->onTimedOut, [&](){
+      if(client->write("hello") == 0)
+        cout << "written" << endl;
+    });
 
+  client->connect("127.0.0.1", 63773);
   client->start();
   timer->start();
 

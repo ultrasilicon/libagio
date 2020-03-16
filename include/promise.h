cc71e9cd106ea13718f9de02492764ef8693f6e0
ultrasilicon <zhenghanecho@gmail.com>
2020-03-16 00:42:40 -0400

    Added some basic function prototypes

--- include/promise.h
+++ include/promise.h
@@ -6,12 +6,41 @@
 
 A_NS_BEGIN
 
-template <typename Ret>
-struct Promise;
+template<typename... Ts>
+class Promise;
 
 
-template <typename Ret>
-struct Promise {
+
+
+template<typename... Ts>
+class Promise {
+public:
+  using T = typename std::tuple_element<0, std::tuple<Ts...> >::type;
+
+  template<typename U = T, class std::enable_if <!std::is_void<U>::value, int >::type = 0>
+  void resolve(U& t) {
+
+  }
+
+  template<typename U = T, class std::enable_if <std::is_void<U>::value, int >::type = 0>
+  void resolve() {
+
+  }
+
+  Promise<T>& then() {
+
+  }
+
+  Promise<T>& finally() {
+
+  }
+
+  Promise<T>& err() {
+
+  }
+
+private:
+  Promise<T>* next_promise_;
 
 };
 
